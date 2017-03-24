477f25f4074c74fc5ad05e3bb2d9c4fcac66209a
Antony Alkmim <antony-alkmim@hotmail.com>
2017-03-24 17:30:16 -0300

    setup gulp-sass

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,21 @@
 const gulp = require("gulp");
 const sourcemaps = require("gulp-sourcemaps");
 const babel = require("gulp-babel");
-// const concat = require("gulp-concat");
+const concat = require("gulp-concat");
 const uglify = require('gulp-uglify');
+const uglifycss = require('gulp-uglifycss');
+const sass = require('gulp-sass');
 
+//ES6 plugins
 const babelify = require('babelify');
 const browserify = require('browserify');
 const buffer = require('vinyl-buffer');
 const source = require('vinyl-source-stream');
 const ngAnnotate = require('gulp-ng-annotate');
 
+
+//Scrips - Transpile all ES6 Javascript files
+//==========================================
 gulp.task("scripts", function () {
     let bundler = browserify({
         entries: 'src/app.js',
@@ -28,19 +34,24 @@ gulp.task("scripts", function () {
 });
 
 
-gulp.task("tests", function () {
-    return gulp.src(["src/**/*.spec.js"])
-        .pipe(babel())
-        .pipe(gulp.dest("tests"));
+// SASS - Compile and concat all Sass files
+//==========================================
+gulp.task("sass", function () {
+    return gulp.src(['src/**/*.scss'])
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(concat('all.min.css'))
+        .pipe(gulp.dest('dist'));
 });
+
 
 //Watch - Rerun the task when a file changes
 //==========================================
 gulp.task('watch', function() {
     gulp.watch(["src/**/*.js", "!src/**/*.spec.js"], ['scripts']);
     gulp.watch("src/**/*.spec.js", ['tests']);
+    gulp.watch("src/**/*.scss", ['sass']);
 });
 
 
 //default
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch', 'scripts', 'sass']);
