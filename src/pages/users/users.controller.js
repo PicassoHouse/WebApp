e34667ca39b5f7fa4b2770fd60536c148792bf7e
Antony Alkmim <antony-alkmim@hotmail.com>
2017-04-20 12:42:54 -0300

    users/ add user form

--- src/pages/users/users.controller.js
+++ src/pages/users/users.controller.js
@@ -1,15 +1,23 @@
 
 export default class UsersController {
 
-    constructor(Api, toastr) {
+    constructor($state, Api, toastr) {
         "ngInject";
 
         //dependencies
+        this.$state = $state;
         this.Api = Api;
         this.toastr = toastr;
 
         //pproperties
         this.users = []; 
+        this.userModel = {
+            username: '',
+            displayName: '',
+            password : '',
+            role: 'guest',
+            auth_code : ''
+        };
 
         //initialize
         this.loadUsers();
@@ -27,6 +35,14 @@ export default class UsersController {
                 this.users = this.users.filter(el => el != user);
                 this.toastr.success('Usuário removido com sucesso!');
             }).catch(err => this.toastr.error('Não foi possível remover usuário!'));
+    }
+
+    addUser(userModel) {
+        this.Api.all('users').post(userModel).then(res => {
+                this.toastr.success("Usuário salvo com sucesso!");
+                this.$state.reload('app.users');
+            })
+            .catch(err => this.toastr.error(err));
     }
 
 }
